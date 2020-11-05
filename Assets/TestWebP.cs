201f0be2849d08ec56d81d5e7db194fa3ecf070c
gouzhun <gouzhun@libii.com>
2020-11-05 11:58:10 +0800

    add content

---
+++ Assets/TestWebP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using WebP;
+
+public class TestWebP : MonoBehaviour
+{
+    void Start()
+    {
+        var bytes = Resources.Load<TextAsset>("patter046.webp").bytes;
+        WebP.Error lError;
+        var tex =
+            WebP.Texture2DExt.CreateTexture2DFromWebP(bytes, lMipmaps: false, lLinear: true, lError: out lError);
+
+        if (lError == WebP.Error.Success)
+        {
+            GetComponent<RawImage>().texture = tex;
+        }
+        else
+        {
+            Debug.LogError("Webp Load Error : " + lError.ToString());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/unity.webp/Editor/Tools.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace WebP.Editor
+{
+    public class Tools
+    {
+        private static string fileStr = "\t#include \"../WebGL/webp_src~{0}\"\n";
+
+
+        [MenuItem("WebP/Gen Source File String")]
+        public static string GenerateSourceFileList()
+        {
+            var folder = EditorUtility.OpenFolderPanel("Select Source Folder", "webp_src~", "");
+            var files = Directory.GetFiles(folder, "*.*", SearchOption.AllDirectories);
+            var sb = new StringBuilder();
+            var dic = new Dictionary<string,string>();
+            foreach (var file in files)
+            {
+                if (!file.EndsWith(".h"))
+                {
+                    continue;
+                }
+
+                
+                var y = file.Replace("\\", "/");
+                var i = y.IndexOf("webp_src~");
+                var key = y.Split('/').Last();
+                if (dic.ContainsKey(key))
+                {
+                    continue;
+                }
+                
+                dic[key] = y;
+                var f = y.Substring(i + "webp_src~".Length);
+                sb.AppendFormat(fileStr, f);
+                Debug.Log(f);
+            }
+
+            var str = "extern \"C\" {\n" + sb.ToString() + "\n}";
+
+            Debug.Log(str);
+            File.WriteAllText(EditorUtility.SaveFilePanelInProject("WebP File", "webp", "cpp", "webgl lib"), str);
+            return null;
+        }
+    }
+}
